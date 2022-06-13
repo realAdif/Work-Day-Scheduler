892ea6b1bfbc144b620ab27559925ea96a16da6f
Aditya <adityagaikwad693@gmail.com>
2022-06-14 00:50:53 +1000

    Added hours to the HTML and current hour

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-alert("this works")
+var today = moment();
+var currentHour = moment().format("h");
+
+$("#currentDay").text(today.format("dddd, MMMM M"));
+
+$("#9").addClass("future");
+$("#10").addClass("future");
+$("#11").addClass("future");
+$("#12").addClass("future");
+$("#1").addClass("future");
+$("#2").addClass("future");
+$("#3").addClass("future");
+$("#4").addClass("future");
+$("#5").addClass("future");
+
+
+
+
+
