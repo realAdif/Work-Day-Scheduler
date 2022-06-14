fdcb3852f5e09295bf0bb1fd93903878d40d7e22
Aditya <adityagaikwad693@gmail.com>
2022-06-14 17:36:43 +1000

    Color Time is working and LocalStorage is working

--- script.js
+++ script.js
@@ -1,18 +1,49 @@
 var today = moment();
-var currentHour = moment().format("h");
+var time = moment().format("h:m ");
+var currentTime = moment().hour();
+var curentBlock = $(".time-block");
+var sec;
+var saveButton= $(".saveBtn");
 
 $("#currentDay").text(today.format("dddd, MMMM M"));
 
-$("#9").addClass("future");
-$("#10").addClass("future");
-$("#11").addClass("future");
-$("#12").addClass("future");
-$("#1").addClass("future");
-$("#2").addClass("future");
-$("#3").addClass("future");
-$("#4").addClass("future");
-$("#5").addClass("future");
+$(function(){
+    setInterval(function(){
+        sec = moment().format("h: mm ss A");
+        $("#curent-time").text(sec)
+    
+    },1000)
 
+    curentBlock.each(function(){
+        var currentHour = parseInt($(this).attr("id"));
+        console.log(currentHour)        
+        if(currentHour > currentTime){
+            $(this).addClass("future");
+        }else if(currentHour < currentTime){
+            $(this).addClass("past");
+        }else{
+            $(this).addClass("present");
+        }    
+    })
+
+    saveButton.on("click", function(){
+        var timeSave = $(this).siblings(".hour").text();
+        var description = $(this).siblings(".description").val();
+
+        localStorage.setItem(timeSave,description);
+
+    });
+
+    $(".hour").each(function(){
+        var hour = $(this).text();
+        console.log(this);
+        var textBox = localStorage.getItem(hour)
+
+        $(this).siblings(".description").val(textBox);
+    })
+
+
+})
 
 
 
